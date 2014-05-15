11e7233ff12f46393c6d5ef5c74aa82381e2a281
CS491 <myork@cs.ua.edu>
2014-05-15 09:07:44 -0500

    - Initial commit

---
+++ TheRestOfTheStory/TheRestOfTheStory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheRestOfTheStory
+//
+//  Created by CS491 on 5/15/14.
+//  Copyright (c) 2014 MattIsTheGreatest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TheRestOfTheStory/TheRestOfTheStoryTests/TheRestOfTheStoryTests.m
@@ -0,0 +1,34 @@
+//
+//  TheRestOfTheStoryTests.m
+//  TheRestOfTheStoryTests
+//
+//  Created by CS491 on 5/15/14.
+//  Copyright (c) 2014 MattIsTheGreatest. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TheRestOfTheStoryTests : XCTestCase
+
+@end
+
+@implementation TheRestOfTheStoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
